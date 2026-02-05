b80f343a76a397397655bddc63f229ed759ed777
asif-wego <asif.firdos@wego.com>
2026-02-05 20:08:05 +0500

    updated

--- run-backend.bat
+++ run-backend.bat
@@ -6,4 +6,5 @@ set POS_DB_PATH=.\data\pos.db
 set GRADLE_OPTS=-Xms64m -Xmx256m -XX:MaxMetaspaceSize=128m
 
 cd /d "%~dp0backend"
+call .\gradlew.bat --no-daemon clean build -x test
 call .\gradlew.bat --no-daemon bootRun
